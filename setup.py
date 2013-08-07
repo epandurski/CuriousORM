8ae1a60cb2c77ae26827a5f0488b9f302a86fa21
Evgeni Pandurski <epandurski@gmail.com>
2013-08-07 10:42:05 +0300

    Updated version number

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ that return tables.
 """
 
 setup(name='CuriousORM',
-      version='1.1.1',
+      version='1.1.2',
       description='Simple object-relational mapper for Python and PostgreSQL',
       maintainer="Evgeni Pandurski",
       maintainer_email="epandurski@gmail.com",
